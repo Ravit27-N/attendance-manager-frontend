f4ca76c606781ab146bc01c1c57b8dec042f3a71
RavitHun <77621123+Ravit27-N@users.noreply.github.com>
2022-12-14 17:32:08 +0700

    Update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,70 +1,115 @@
 <template>
-    <v-app id="inspire">
-      <v-content>
-        <v-container class="fill-height" fluid>
-          <v-row align="center" justify="center">
-            <v-col cols="12" sm="8" md="8">
-              <v-card class="elevation-12">
-                <v-window v-model="step">
-                  <v-window-item :value="1">
-                    <v-row>
-                      <v-col cols="8" md="4" class="gray">
-                        <v-card-text class=" mt-12">
-                          <h3 class="text-center display-1">Welcome Back!</h3>
-                          <h3
-                            class="text-center"
-                          >Login and start engaging your data!</h3>
-                        </v-card-text>
-                        
-                      </v-col>
-                      <v-col cols="12" md="8" class="blue">
-                        <v-card-text class="mt-12">
-                          <h1
-                            class="text-center "
-                          >USERLOGIN</h1>
-                        
-                          <v-form>
-                            <v-text-field
-                              label="Email"
-                              name="Email"
-                              prepend-icon="email"
-                              type="text"
-                              color="teal accent-3"
-                            />
-  
-                            <v-text-field
-                              id="password"
-                              label="Password"
-                              name="password"
-                              prepend-icon="lock"
-                              type="password"
-                              color="teal accent-3"
-                            />
-                          </v-form>
-                          <h3 class="text-left mt-4">Forgot your password ?</h3>
-                        </v-card-text>
-                        <div class="text-center mt-3">
-                          <v-btn rounded color="white" >Log In</v-btn>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height contain-image" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8">
+            <v-card class="elevation-12">
+              <v-window v-model="step">
+                <v-window-item :value="1">
+                  <v-row>
+                    <v-col cols="12" md="6" class="gray">
+                      <v-card-text class="mt-12">
+                        <h3 class="title-image welcome display-1">Welcome Back!</h3>
+                        <h3 class="title-image">Login and start engaging your data!</h3>
+                        <div class="image-login">
+                          <img class="image" src="../assets/Login.png" alt />
                         </div>
-                      </v-col>
-                    </v-row>
-                  </v-window-item>
-                </v-window>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-content>
-    </v-app>
-  </template>
-  
+                      </v-card-text>
+                    </v-col>
+                    <v-col cols="12" md="6" class="contain-login">
+                      <v-card-text class="mt-12">
+                        <div class="text-center">
+                          <img src="../assets/user.png" alt />
+                        </div>
+                        <h1 class="text-center login-title">USERLOGIN</h1>
+                        <div class="form-input">
+                          <label for="Email">Email</label>
+                          <v-text-field
+                            solo
+                            label="Enter your email"
+                            name="Email"
+                            append-icon="mdi-email"
+                            type="email"
+                            outlined
+                            clearable
+                          ></v-text-field>
+                          <label for="Email">Password</label>
+                          <v-text-field
+                            solo
+                            label="Enter your password"
+                            append-icon="mdi-lock"
+                            outlined
+                            type="password"
+                            clearable
+                          ></v-text-field>
+                         
+                          
+                        </div>
+                        <a href class="text-left forgotlink">Forgot password?</a>
+                      </v-card-text>
+                      <div class="text-center mt-3">
+                        <v-btn rounded color="white" class="btn-login">Log In</v-btn>
+                      </div>
+                    </v-col>
+                  </v-row>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+ 
   <script>
-  export default {
-    data: () => ({
-      step: 1
-    }),
-    props: {
-      source: String
-    }
-  };
-  </script>
+export default {
+  data: () => ({
+    step: 1
+  }),
+  props: {
+    source: String
+  }
+};
+</script>
+<style scoped>
+.title-image {
+  margin-left: 1rem;
+  color: #0062e0;
+}
+.welcome {
+  font-size: 2rem !important;
+  font-weight: bold;
+}
+.contain-login {
+  background-color: #0062e0;
+}
+.contain-login img {
+  width: 5vw;
+  margin-bottom: 0.3rem;
+}
+.btn-login {
+  margin-bottom: 3rem;
+}
+.image {
+  width: 30vw;
+}
+.login-title {
+  color: white;
+}
+.forgotlink {
+  text-align: left;
+  color: white;
+  text-decoration: none;
+}
+.form-input{
+  margin-top: 1rem;
+}
+.form-input label{
+  color: white;
+  font-size: 1rem;
+  margin-left: 0.3rem;
+}
+
+</style>
---
+++ src/views/Test.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="contain">
+    <div class="image-contain">
+      <h2>Welcome Back!</h2>
+      <h3>Login and start engaging your data!</h3>
+      <img class="image" src="../assets/Login.png" alt />
+    </div>
+    <div class="fill-contain">
+      <div cols="12" md="8" class="contain-login">
+        <v-card-text class="mt-12">
+          <h1 class="text-center">USERLOGIN</h1>
+
+          <v-form>
+            <v-text-field
+              label="Email"
+              name="Email"
+              prepend-icon="email"
+              class="field-input"
+              type="text"
+              color="white accent-3"
+            />
+
+            <v-text-field
+              id="password"
+              label="Password"
+              name="password"
+              class="field-input"
+              prepend-icon="lock"
+              type="password"
+              color="white accent-3"
+            />
+          </v-form>
+          <h3 class="text-left mt-4">Forgot your password ?</h3>
+        </v-card-text>
+        <div class="text-center mt-3">
+          <v-btn rounded color="white" class="btn-login">Log In</v-btn>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.contain {
+  width: 100%;
+  background-color: aqua;
+  display: flex;
+}
+.image-contain {
+  width: 40%;
+}
+.image-contain .img {
+  width: 90%;
+}
+</style>
